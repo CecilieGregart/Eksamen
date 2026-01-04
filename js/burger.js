285d3a12e2b550bd07d699553b6dcfcc66a0f392
Cecilie <cegre0001@stud.ek.dk>
2026-01-04 20:25:41 +0100

    Tilføjer js

---
+++ js/burger.js
@@ -0,0 +1,9 @@
+const burger = document.querySelector(".burger");
+const nav = document.querySelector(".nav");
+
+burger.addEventListener("click", () => {
+  burger.classList.toggle("active");
+  nav.classList.toggle("active");
+
+  burger.setAttribute("aria-expanded", burger.classList.contains("active"));
+});
